fea39626093965cf2f5ad2aab25459134550371a
Donald Davis <donald.m.davis@sherwin.com>
2017-10-27 11:39:43 -0400

    setting up entity framework

---
+++ Ceramics/Data/ClassContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ceramics.Data
+{
+    public class ClassContext : DbContext
+    {
+        public int Id { get; set; }
+        public List<StudentModel> Students { get; set; }
+        public bool IsCancelled { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
---
+++ Ceramics/Models/DataModels/StudentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Ceramics.Models.DataModels
+{
+    public class StudentModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool GoodStanding { get; set; }
+    }
+}
