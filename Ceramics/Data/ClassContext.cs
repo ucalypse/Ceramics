735cdd2f80a82ae8bc2f9a054050d7cd46ec467d
Donald Davis <donald.m.davis@sherwin.com>
2017-10-31 15:48:51 -0400

    added bulma reference

--- Ceramics/Data/ClassContext.cs
+++ Ceramics/Data/ClassContext.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Ceramics.Models.DataModels;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ceramics.Data
