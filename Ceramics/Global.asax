cd83f49beb7b720b73ed187810b3f5d011fd59b5
Davis <I813096@ngic.com>
2017-12-06 15:18:31 -0500

    initial commit

---
+++ Ceramics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ceramics.MvcApplication" Language="C#" %>
---
+++ Ceramics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Ceramics.Startup))]
+namespace Ceramics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
